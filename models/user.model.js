b18ee8464111ca6192a6d6d7203383da3bf4b216
William Goode <wgoode3@gmail.com>
2020-10-11 16:47:31 -0400

    validations / some css

--- models/user.model.js
+++ models/user.model.js
@@ -4,11 +4,19 @@ const sequelize = require("../config/sequelize.config");
 const User = sequelize.define("User", {
   username: {
     type: DataTypes.STRING(255),
-    allowNull: false
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      len: [2, 255]
+    }
   },
   email: {
     type: DataTypes.STRING(255),
-    allowNull: false
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      isEmail: true
+    }
   }
 }, {
   timestamps: true
